3286a2a231d58399c5f7f099f51228f1bf122831
直井　真一郎 <naoi@qri.jp>
2016-08-03 17:49:30 +0900

    Initial Commit

---
+++ mylearning/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  mylearning
+//
+//  Created by 直井　真一郎 on 2016/08/03.
+//  Copyright © 2016年 Shinichiro Naoi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
